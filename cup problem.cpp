b85d83ccb8f75d03486c38269764816f4173c8e7
Priyanka Singh <88391140+priyankasingh1218@users.noreply.github.com>
2022-01-20 16:26:30 +0530

    Add files via upload
    we have taken "a" number of cups and we have assume that two users are playing this game.user who picks the last cup wins the game.

---
+++ cup problem.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"numbers of cups are\n";
+	cin>>a;
+	if(a%2==0)
+	  cout<<"user 2 won\n";
+	else
+	  cout<<"user 1 won\n";
+	return 0;
+}
